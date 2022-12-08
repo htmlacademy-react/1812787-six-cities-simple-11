7ffa4ce4535a4158ee9ce4e61779874d9073ce18
lizatalkina <liza.talkina@gmail.com>
2022-12-08 12:56:40 +0300

    Добавляет аутентификацию

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AppRoute, MAP_CLASS, CITIES, SORTING_OPTIONS } from '../../const';
+import { MAP_CLASS, CITIES, SORTING_OPTIONS } from '../../const';
 import PlaceList from '../../components/place-list/place-list';
 import CitiesList from '../../components/cities-list/cities-list';
 import Map from '../../components/map/map';
@@ -8,6 +7,7 @@ import MainEmpty from '../../components/main-empty/main-empty';
 import SortingOptions from '../../components/sorting-options/sorting-options';
 import type { Hotel } from '../../types/hotels';
 import { useAppSelector } from '../../hooks';
+import Header from '../../components/header/header';
 
 function MainScreen(): JSX.Element {
   const [selectedHotel, setSelectedHotel] = useState<Hotel | undefined>(undefined);
@@ -58,32 +58,7 @@ function MainScreen(): JSX.Element {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link to={AppRoute.Root} className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <div className="header__nav-profile">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </div>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header/>
 
       <main className={ !isEmpty ? 'page__main page__main--index' : 'page__main page__main--index page__main--index-empty'}>
         <h1 className="visually-hidden">Cities</h1>
--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,9 +1,12 @@
-import { AxiosInstance } from 'axios';
+import { AxiosInstance, AxiosResponse } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getOffers, setOffersDataLoadingStatus } from './action';
+import { getOffers, requireAuthorization, setOffersDataLoadingStatus, setUserEmail, redirectToRoute } from './action';
 import { AppDispatch, State } from '../types/state.js';
 import { Hotel } from '../types/hotels';
-import { APIRoute } from '../const';
+import { APIRoute, AuthorizationStatus, AppRoute } from '../const';
+import { AuthData } from '../types/auth-data';
+import { UserData } from '../types/user-data';
+import { dropToken, saveToken } from '../services/token';
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -18,5 +21,52 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
     dispatch(getOffers({
       offers: data
     }));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try{
+      const {data: {email: userEmail}}: AxiosResponse<UserData> = await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+      dispatch(setUserEmail(userEmail));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+      dispatch(setUserEmail(null));
+    }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async({login: email, password}, {dispatch, extra: api}) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(setUserEmail(email));
+    dispatch(redirectToRoute(AppRoute.Root));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/logout',
+  async (_arg, {dispatch, extra: api}) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    dispatch(setUserEmail(null));
   },
 );
